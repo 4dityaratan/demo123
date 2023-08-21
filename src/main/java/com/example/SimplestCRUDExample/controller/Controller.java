c37590654e2286c72d767f69262c4c23a2108ab8
4dityaratan <aditya.jcro@gmail.com>
2023-08-21 14:17:20 +0530

    added

---
+++ src/main/java/com/example/SimplestCRUDExample/controller/Controller.java
@@ -0,0 +1,10 @@
+package com.example.SimplestCRUDExample.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+
+}
